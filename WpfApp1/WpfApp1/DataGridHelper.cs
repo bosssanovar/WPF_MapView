c879fd248edc30934f8c91e5dd9d8f8c45eb4374
kei ito <ito.kei@jvckenwood.com>
2024-02-22 08:57:27 +0900

    first commit

---
+++ WpfApp1/WpfApp1/DataGridHelper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace WpfApp1
+{
+    internal static class DataGridHelper
+    {
+        public static int GetSelectedColumnIndex(DataGrid grid)
+        {
+            return grid.CurrentCell.Column.DisplayIndex;
+        }
+
+        public static int GetSelectedRowIndex(DataGrid grid)
+        {
+            return grid.Items.IndexOf(grid.CurrentItem);
+        }
+
+        public static DataGridCell? GetCellAtMousePosition(object sender, MouseButtonEventArgs e)
+        {
+            var hit = VisualTreeHelper.HitTest((Visual)sender, e.GetPosition((IInputElement)sender));
+            DependencyObject cell = VisualTreeHelper.GetParent(hit.VisualHit);
+            while (cell != null && !(cell is System.Windows.Controls.DataGridCell)) cell = VisualTreeHelper.GetParent(cell);
+            System.Windows.Controls.DataGridCell? targetCell = cell as System.Windows.Controls.DataGridCell;
+            return targetCell;
+        }
+
+        public static List<RowColumnIndex> GetSelectedCellsIndex(DataGrid grid)
+        {
+            var ret = new List<RowColumnIndex>();
+
+            var cells = grid.SelectedCells;
+            foreach ( var cell in cells )
+            {
+                ret.Add(new(grid.Items.IndexOf(cell.Item), cell.Column.DisplayIndex));
+            }
+
+            return ret;
+        }
+
+        public class RowColumnIndex(int rowIndex, int columnIndex)
+        {
+            public int RowIndex { get; } = rowIndex;
+            public int ColumnIndex { get; } = columnIndex;
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/MainWindow2.xaml.cs
@@ -0,0 +1,174 @@
+﻿using Reactive.Bindings;
+using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow2 : Window
+    {
+        private const int InitCulumnCount = 640;
+
+        public ObservableCollection<Detail> Items { get; private set; } = new ObservableCollection<Detail>();
+
+        public MainWindow2()
+        {
+            InitializeComponent();
+
+            InitData(InitCulumnCount);
+        }
+
+        protected override void OnContentRendered(EventArgs e)
+        {
+            base.OnContentRendered(e);
+
+            InitColumns(InitCulumnCount);
+
+            grid.Visibility = Visibility.Visible;
+
+            App.Current.MainWindow = this;
+        }
+
+        private void InitColumns(int count)
+        {
+            grid.Columns.Clear();
+
+            var converter = new BooleanToVisibilityConverter();
+            for (int columnIndex = 0; columnIndex < count; ++columnIndex)
+            {
+                var binding = new Binding($"Values[{columnIndex}].Value");
+                binding.Converter = converter;
+
+                var factory = new FrameworkElementFactory(typeof(Rectangle));
+                factory.SetValue(Rectangle.HeightProperty, 10.0);
+                factory.SetValue(Rectangle.WidthProperty, 10.0);
+                factory.SetValue(Rectangle.FillProperty, Brushes.LightSkyBlue);
+                factory.SetBinding(Rectangle.VisibilityProperty, binding);
+
+                var dataTemplate = new DataTemplate();
+                dataTemplate.VisualTree = factory;
+
+                var column = new DataGridTemplateColumn();
+                column.CellTemplate = dataTemplate;
+
+                grid.Columns.Add(column);
+            }
+        }
+
+        private void InitData(int count)
+        {
+            // バインドを切断
+            Binding b = new Binding("Items")
+            {
+                Source = null
+            };
+            grid.SetBinding(DataGrid.ItemsSourceProperty, b);
+
+            var list = new List<Detail>();
+            for (int i = 0; i < count; i++)
+            {
+                list.Add(new Detail(InitCulumnCount));
+            }
+
+            Items = new ObservableCollection<Detail>(list);
+
+            b = new Binding("Items")
+            {
+                Source = this
+            };
+            grid.SetBinding(DataGrid.ItemsSourceProperty, b);
+        }
+
+        private void DataGridCell_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (grid.SelectedCells.Count == 1)
+            {
+                var columnIndex = DataGridHelper.GetSelectedColumnIndex(grid);
+                var rowIndex = DataGridHelper.GetSelectedRowIndex(grid);
+
+                Items[rowIndex].Invert(columnIndex);
+            }
+        }
+
+        private void DataGridCell_PreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (grid.SelectedCells.Count <= 1)
+            {
+                grid.Focus();
+                grid.SelectedCells.Clear();
+
+                DataGridCell? targetCell = DataGridHelper.GetCellAtMousePosition(sender, e);
+
+                if (targetCell is null) return;
+                grid.CurrentCell = new DataGridCellInfo(targetCell);
+                grid.SelectedCells.Add(grid.CurrentCell);
+
+                ShowContextMenu(false);
+            }
+            else
+            {
+                ShowContextMenu(true);
+            }
+        }
+
+        private void ShowContextMenu(bool isSelectArea)
+        {
+            ContextMenu contextMenu = new ContextMenu();
+
+            MenuItem menuItem = new MenuItem();
+            menuItem.Header = "行全部設定";
+            menuItem.Click += new RoutedEventHandler(AllOn);
+            menuItem.IsEnabled = !isSelectArea;
+            contextMenu.Items.Add(menuItem);
+
+            Separator separator = new Separator();
+            contextMenu.Items.Add(separator);
+
+            menuItem = new MenuItem();
+            menuItem.Header = "選択エリア設定";
+            menuItem.Click += new RoutedEventHandler(AreaOn);
+            menuItem.IsEnabled = isSelectArea;
+            contextMenu.Items.Add(menuItem);
+
+            contextMenu.IsOpen = true;
+        }
+
+        private void AllOn(object sender, RoutedEventArgs e)
+        {
+            var rowIndex = DataGridHelper.GetSelectedRowIndex(grid);
+            Items[rowIndex].SetAll(true);
+        }
+
+        private void AreaOn(object sender, RoutedEventArgs e)
+        {
+            var indexes = DataGridHelper.GetSelectedCellsIndex(grid);
+            foreach (var index in indexes)
+            {
+                Items[index.RowIndex].SetOn(index.ColumnIndex);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var next = new MainWindow2();
+            next.Show();
+            App.Current.MainWindow = next;
+
+            this.Close();
+        }
+    }
+}
