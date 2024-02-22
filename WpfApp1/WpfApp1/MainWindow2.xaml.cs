6848e967ce7dad0fabb5adb46fed1ae26d4a15b1
kei ito <ito.kei@jvckenwood.com>
2024-02-22 09:41:22 +0900

    全体マップコントロールを追加

--- WpfApp1/WpfApp1/MainWindow2.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow2.xaml.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -174,6 +175,85 @@ namespace WpfApp1
             App.Current.MainWindow = next;
 
             this.Close();
+        }
+
+
+        private void Thumb_DragStarted(object sender, DragStartedEventArgs e)
+        {
+            var thumb = sender as Thumb;
+            if (null != thumb)
+            {
+                var border = thumb.Template.FindName("Thumb_Border", thumb) as Border;
+                if (null != border)
+                {
+                    border.BorderThickness = new Thickness(1);
+
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void Thumb_DragCompleted(object sender, DragCompletedEventArgs e)
+        {
+            var thumb = sender as Thumb;
+            if (null != thumb)
+            {
+                var border = thumb.Template.FindName("Thumb_Border", thumb) as Border;
+                if (null != border)
+                {
+                    border.BorderThickness = new Thickness(0);
+
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void Thumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            var thumb = sender as Thumb;
+            if (null != thumb)
+            {
+                var x = Canvas.GetRight(thumb) - e.HorizontalChange;
+                var y = Canvas.GetBottom(thumb) - e.VerticalChange;
+
+                var canvas = thumb.Parent as Canvas;
+                if (null != canvas)
+                {
+                    x = Math.Max(x, 0);
+                    y = Math.Max(y, 0);
+                    x = Math.Min(x, canvas.ActualWidth - thumb.ActualWidth);
+                    y = Math.Min(y, canvas.ActualHeight - thumb.ActualHeight);
+                }
+
+                Canvas.SetRight(thumb, x);
+                Canvas.SetBottom(thumb, y);
+
+                e.Handled = true;
+            }
+        }
+
+        private void Thumb_DragDelta2(object sender, DragDeltaEventArgs e)
+        {
+            var thumb = sender as Thumb;
+            if (null != thumb)
+            {
+                var x = Canvas.GetLeft(thumb) + e.HorizontalChange;
+                var y = Canvas.GetTop(thumb) + e.VerticalChange;
+
+                var canvas = thumb.Parent as Canvas;
+                if (null != canvas)
+                {
+                    x = Math.Max(x, 0);
+                    y = Math.Max(y, 0);
+                    x = Math.Min(x, canvas.ActualWidth - thumb.ActualWidth);
+                    y = Math.Min(y, canvas.ActualHeight - thumb.ActualHeight);
+                }
+
+                Canvas.SetLeft(thumb, x);
+                Canvas.SetTop(thumb, y);
+
+                e.Handled = true;
+            }
         }
     }
 }
